3d986cf997130380a528a13257735e68f66a9d97
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2021-06-29 01:39:41 -0400

    added some test logs

--- parsePhoneNumber.js
+++ parsePhoneNumber.js
@@ -7,12 +7,39 @@ function parsePhoneNumber(n){
     }
     if(length > 2){
         result += `)-${scrubbedN.slice(3,6)}`
-
     }
     if(length > 6){
         result += `-${scrubbedN.slice(6,10)}`
-
     }
     return result
-    
 }
+
+function test(s){
+    console.log(parsePhoneNumber(s))
+}
+
+let data = [
+    '1234567890',
+    '123456789',
+    '12345678',
+    '1234567',
+    '123456',
+    '12345',
+    '1234',
+    '123',
+    '12',
+    '1',
+    '123a4567890',
+    '1234aa56789',
+    '1234a5678',
+    '123asd4567',
+    '1234asd56',
+    '1234_+5',
+    '123=4',
+    '12-3',
+    '12()',
+    '!@#$%^&*(1'
+
+]
+
+data.forEach(test)
